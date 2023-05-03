544dbe8954124fc91c9145836dd1b73e971aadca
Anshuman Swain <anshuman8swain@gmail.com>
2023-05-04 00:30:11 +0530

    task 2 basic done

--- Task-02/script.js
+++ Task-02/script.js
@@ -33,8 +33,14 @@ function updateColor() {
   var greenValue = document.getElementById("green-slider").value;
   var blueValue = document.getElementById("blue-slider").value;
   var colorString = "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
-
   // Todo: Update the text color
+  document.getElementById("work").style.color=colorString;
+  document.getElementById("life").style.color=colorString;
+  document.getElementById("balance").style.color=colorString;
+  document.getElementById("red-value").innerHTML=redValue;
+  document.getElementById("green-value").innerHTML=greenValue;
+
+
 
 }
   
