b39530d697fd55222276003a25f126a98794734d
rithulr808 <rithulravichnadra@gmail.com>
2023-05-04 05:53:28 +0530

    count_Till_3_JS

--- Task-01/Code/script.js
+++ Task-01/Code/script.js
@@ -1,1 +1,17 @@
 // write code for your buttons here.
+
+count = 0;
+function countFunction() {
+    console.log("count");
+    const countDisplay = document.getElementById("countDisplay");
+    
+    if (count == 3){
+        count = 0;
+    }
+    else{
+        count = count+1;
+    }
+    countDisplay.innerHTML = count;
+
+  }
+
