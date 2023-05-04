6537c07a4af8117b603f13c307fd4e9a5e62cdd6
madhav <madhavadas31@gmail.com>
2023-05-04 08:49:24 +0530

    designed

--- Task-04/script.js
+++ Task-04/script.js
@@ -1,21 +1,69 @@
-const root = document.querySelector('html');
+const poke_container = document.getElementById('poke_container');
+const pokemons_number = 150;
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
+const main_types = Object.keys(colors);
 
+const fetchPokemons = async () => {
+	for (let i = 1; i <= pokemons_number; i++) {
+		await getPokemon(i);
+	}
+};
 
-const cursor = document.createElement('div');
-cursor.classList.add('cursor');
-root.appendChild(cursor);
+const getPokemon = async id => {
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+	const res = await fetch(url);
+	const pokemon = await res.json();
+	createPokemonCard(pokemon);
+};
 
+function createPokemonCard(pokemon) {
+	const pokemonEl = document.createElement('div');
+	pokemonEl.classList.add('pokemon');
 
-const follower = document.createElement('div');
-follower.classList.add('cursor', 'cursor__follower');
-root.appendChild(follower);
+	const poke_types = pokemon.types.map(type => type.type.name);
+	const type = main_types.find(type => poke_types.indexOf(type) > -1);
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+	const color = colors[type];
+	
+	pokemonEl.style.backgroundColor = color;
 
+	const pokeInnerHTML = `
+        <div class="img-container">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+							pokemon.id
+						}.png" alt="${name}" />
+        </div>
+        <div class="info">
+            <span class="number">#${pokemon.id
+							.toString()
+							.padStart(3, '0')}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>${type}</span></small>
+        </div>
+    `;
 
-root.addEventListener('mousemove', e => {
-  setPosition(follower, e);
-  setPosition(cursor, e);
-});
+	pokemonEl.innerHTML = pokeInnerHTML;
 
-function setPosition(element, e) {
-  element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
+	poke_container.appendChild(pokemonEl);
 }
+
+fetchPokemons();
+
+
+
+
